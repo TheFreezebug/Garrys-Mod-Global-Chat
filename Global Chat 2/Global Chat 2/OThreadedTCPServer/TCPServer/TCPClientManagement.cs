c2c66dd65db25f299169a894c0aad895e9d2a29c
Dane Z. Bush <danebush45@yahoo.com>
2015-05-05 21:08:41 -0400

    Fixed a bunch of stuff

---
+++ Global Chat 2/Global Chat 2/OThreadedTCPServer/TCPServer/TCPClientManagement.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Sockets;
+using System.Threading;
+using System.Net;
+using System.Runtime.InteropServices;
+
+
+namespace Global_Chat_2
+{
+
+    public partial class TCPServer
+    {
+
+        private void AddClient(TcpClient cli)
+        {
+            for (int I = 0; I < ConnectedClients.Length; I++)
+            {
+
+                var cli2 = ConnectedClients[I];
+
+                if (cli2 == null)
+                {
+                    ConnectedClients[I] = cli;
+                    donut.outlc("Successfully added client in slot " + I, ConsoleColor.Green);
+                    return;
+                }
+
+            }
+
+        }
+
+
+        private void PruneClients()
+        {
+
+            for (int I = 0; I < ConnectedClients.Length; I++)
+            {
+
+                var cli2 = ConnectedClients[I];
+                try
+                {
+                    if (cli2 != null)
+                    {
+                        if (cli2.Client == null)
+                        {
+                            ConnectedClients[I] = null;
+                            donut.outlc("Killed bad client!", ConsoleColor.Red);
+                        }
+                        else if ((cli2.Client.Poll(1, SelectMode.SelectRead) && cli2.Client.Available == 0))
+                        {
+                            donut.outlc("Killed bad client! 2 ", ConsoleColor.Red);
+                            ConnectedClients[I] = null;
+                        }
+                    }
+                }
+                catch
+                {
+                    donut.outlc("Killed bad client! 3 ", ConsoleColor.Red);
+                    ConnectedClients[I] = null;
+                }
+
+            }
+
+        }
+
+        private int GetClientsCount()
+        {
+            int cc = 0;
+            for (int I = 0; I < ConnectedClients.Length; I++)
+            {
+
+                var cli2 = ConnectedClients[I];
+                if (cli2 != null)
+                {
+                    cc++;
+                }
+            }
+            return cc;
+        }
+
+
+        #region Remove Client Function
+        private void RemoveClient(TcpClient cl, int why) // waste of a method sorry.
+        {
+            try
+            {
+                for (int I = 0; I < ConnectedClients.Length; I++)
+                {
+                    var cli2 = ConnectedClients[I];
+                    if (cl == cli2)
+                    {
+                        donut.outlc("Removed client successfully.", ConsoleColor.Red);
+                        ConnectedClients[I] = null;
+
+                    }
+                }
+            }
+            catch (Exception EXC)
+            {
+                donut.outlc("Can't remove client from Connected list, weird shit may happen.", ConsoleColor.DarkYellow);
+                donut.outlc(EXC.ToString(), ConsoleColor.DarkYellow);
+                donut.outlc("!!!!!!!!!!!!!!!!!!!!", ConsoleColor.Red);
+
+            }
+
+            try
+            {
+                cl.Close();
+                cl = null;
+            }
+            catch
+            {
+
+            }
+
+            PruneClients();
+
+        }
+
+
+
+
+
+
+
+
+        
+
+    }
+}
--- Global Chat 2/Global Chat 2/OThreadedTCPServer/TCPServer/TCPServer.cs
+++ Global Chat 2/Global Chat 2/OThreadedTCPServer/TCPServer/TCPServer.cs
@@ -11,11 +11,13 @@ using System.Threading.Tasks;
 using System.Net.Sockets;
 using System.Threading;
 using System.Net;
+using System.Runtime.InteropServices;
+
 
 namespace Global_Chat_2
 {   
    
-   public partial class TCPServer
+   public partial class TCPServer 
     {
 
         #region Variables and enums
@@ -32,7 +34,7 @@ namespace Global_Chat_2
 
             private static TcpClient[] ConnectedClients; // The current connected clients.
 
-            private Dictionary<string, string> Blacklist = new Dictionary<string, string>(); // Who is not allowed to connect.
+           // private Dictionary<string, string> Blacklist = new Dictionary<string, string>(); // Who is not allowed to connect.
 
             private bool running = false; // is the server already started.
 
@@ -90,126 +92,27 @@ namespace Global_Chat_2
                 return (int)Codes.FAIL_START;
             }
             ConnectedClients =  new TcpClient[maxclients];
-            //donut.outlc("Successfully started TCP Server.", ConsoleColor.DarkYellow);
+       
             ConnectorThread = new Thread(new ThreadStart(clientConnectSpin)); 
             ConnectorThread.Start();
             return (int)Codes.SUCCESS_START;
         }
             #endregion
 
-        private void AddClient(TcpClient cli)
+        public int Stop()
         {
+            ConnectorThread.Abort(); // Stop accepting connections.
+            PruneClients();
             for (int I = 0; I < ConnectedClients.Length; I++)
             {
-
-                var cli2 = ConnectedClients[I];
-
-                if (cli2 == null)
+                TcpClient KCli = ConnectedClients[I];
+                if (KCli != null)
                 {
-                    ConnectedClients[I] = cli;
-                    donut.outlc("Successfully added client in slot " + I,ConsoleColor.Green);
-                    return;
+                    KCli.Close();
                 }
-
             }
-           
+                return 1;
         }
-
-
-        private void PruneClients()
-        {
-
-            for (int I = 0; I < ConnectedClients.Length;  I++)
-            {
-
-                var cli2 = ConnectedClients[I];
-                try
-                {
-                    if (cli2 != null)
-                    {
-                        if (cli2.Client == null)
-                        {
-                            ConnectedClients[I] = null;
-                            donut.outlc("Killed bad client!", ConsoleColor.Red);
-                        }
-                        else if ((cli2.Client.Poll(1, SelectMode.SelectRead) && cli2.Client.Available == 0))
-                        {
-                            donut.outlc("Killed bad client! 2 ", ConsoleColor.Red);
-                            ConnectedClients[I] = null;
-                        }
-                    }
-                }
-                catch
-                {
-                    donut.outlc("Killed bad client! 3 ", ConsoleColor.Red);
-                    ConnectedClients[I] = null;
-                }
-
-            }
-            
-        }
-
-        private int GetClientsCount()
-        {
-            int cc = 0;
-            for (int I = 0; I < ConnectedClients.Length;  I++)
-            {
-
-                var cli2 = ConnectedClients[I];
-                if (cli2!=null) {
-                    cc++;
-                }
-             }
-            return cc;
-        }
-
-
-        #region Remove Client Function
-        private void RemoveClient(TcpClient cl, int why) // waste of a method sorry.
-        {
-            try
-            {
-                for (int I = 0; I < ConnectedClients.Length; I++)
-                {
-                    var cli2 = ConnectedClients[I];
-                    if (cl == cli2)
-                    {
-                        donut.outlc("Removed client successfully.", ConsoleColor.Red);
-                        ConnectedClients[I] = null;
-
-                    }
-                }
-            }
-            catch (Exception EXC)
-            {
-                donut.outlc("Can't remove client from Connected list, weird shit may happen.", ConsoleColor.DarkYellow);
-                donut.outlc(EXC.ToString(), ConsoleColor.DarkYellow);
-                donut.outlc("!!!!!!!!!!!!!!!!!!!!", ConsoleColor.Red);
-                
-            }
-
-            try
-            {
-                cl.Close();
-                cl = null;
-            }
-            catch
-            {
-
-            }
-
-            PruneClients();
-           
-        }
-
-
-
-
-
-
-
-
-        #endregion 
 
 
 
